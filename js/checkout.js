cca96066dd9127fa07406072a03fa4ea2140dcaf
Mesadova <emesadova@hotmail.com>
2024-10-09 13:45:53 +0200

    Fix checkout form

--- js/checkout.js
+++ js/checkout.js
@@ -1,60 +1,70 @@
 
 // Exercise 6
 function validate() {
-	var error = 0;
-	var regEmail = /^([a-zA-Z0-9_\-\.])+\@([a-zA-Z0-9_\-\.])+\.([a-zA-Z]{2,})$/;
-	var regName = /^[a-zA-Z]{3,}$/;
-	var regPhone = /^[0-9]{3,}$/;
-	var regAddress = /^[a-zA-Z0-9]{3,}$/;
-	var regPassword = /^(?=.*?\d)(?=.*?[a-zA-Z])[a-zA-Z\d]{4,}$/;
+	let error = 0;
+	let regEmail = /^([a-zA-Z0-9_\-\.])+\@([a-zA-Z0-9_\-\.])+\.([a-zA-Z]{2,})$/;
+	let regName = /^[a-zA-Z]{3,}$/;
+	let regPhone = /^[0-9]{3,}$/;
+	let regAddress = /^[a-zA-Z0-9]{3,}$/;
+	let regPassword = /^(?=.*?\d)(?=.*?[a-zA-Z])[a-zA-Z\d]{4,}$/;
 
 	// Get the input fields
-	var fName = document.getElementById("fName");
-	var fLastN = document.getElementById("fLastN");
-	var fEmail = document.getElementById("fEmail");
-	var fPassword = document.getElementById("fPassword");
-	var fAddress = document.getElementById("fAddress");
-	var fPhone = document.getElementById("fPhone");
+	let fName = document.getElementById("fName");
+	let fLastN = document.getElementById("fLastN");
+	let fEmail = document.getElementById("fEmail");
+	let fPassword = document.getElementById("fPassword");
+	let fAddress = document.getElementById("fAddress");
+	let fPhone = document.getElementById("fPhone");
 
 	// Get the error elements
-	var errorName = document.getElementById("errorName");
-	var errorEmail = document.getElementById("errorEmail");
-	var errorLastN = document.getElementById("errorLastN");
-	var errorPassword = document.getElementById("errorPassword");
-	var errorAddress = document.getElementById("errorAddress");
-	var errorPhone = document.getElementById("errorPhone");
+	let errorName = document.getElementById("errorName");
+	let errorEmail = document.getElementById("errorEmail");
+	let errorLastN = document.getElementById("errorLastN");
+	let errorPassword = document.getElementById("errorPassword");
+	let errorAddress = document.getElementById("errorAddress");
+	let errorPhone = document.getElementById("errorPhone");
 	
 	// Validate fields entered by the user: name, phone, password, and email
 	if(fName.value == "" || regName.test(fName.value) == false) {
-		error++;
+		fName.className = 'form-control ml-3 is-invalid';
+		event.preventDefault();
+	} else {
+		fName.className = 'form-control ml-3 is-valid';
 	}
 
 	if(fLastN.value == "" || regName.test(fLastN.value) == false) {
-		error++;
+		fLastN.className = 'form-control ml-3 is-invalid';
+		event.preventDefault();
+	} else {
+		fLastN.className = 'form-control ml-3 is-valid';
 	}
 
 	if(fEmail.value == "" || regEmail.test(fEmail.value) == false) {
-		error++;
+		fEmail.className = 'form-control ml-3 is-invalid';
+		event.preventDefault();
+	} else {
+		fEmail.className = 'form-control ml-3 is-valid';
 	}
 	
 	if(fPhone.value == "" || regPhone.test(fPhone.value) == false) {
-		error++;
+		fPhone.className = 'form-control ml-3 is-invalid';
+		event.preventDefault();
+	} else {
+		fPhone.className = 'form-control ml-3 is-valid';
 	}
 
 	if(fAddress.value == "" || regAddress.test(fAddress.value) == false) {
-		error++;
+		fAddress.className = 'form-control ml-3 is-invalid';
+		event.preventDefault();
+	} else {
+		fAddress.className = 'form-control ml-3 is-valid';
 	}
 
 	if(fPassword.value == "" || regPassword.test(fPassword.value) == false) {
-		error++;
-	}
-
-
-
-	if(error>0){
-		alert("Error");
-	}else{
-		alert("OK");
+		fPassword.className = 'form-control ml-3 is-invalid';
+		event.preventDefault();
+	} else {
+		fPassword.className = 'form-control ml-3 is-valid';
 	}
 
 }
